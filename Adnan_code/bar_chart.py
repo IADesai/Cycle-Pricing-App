e5c02802b83059895a882e1b3d14a8803b922c2f
Adnan Kachwala <115113652+naan73@users.noreply.github.com>
2023-02-03 17:05:56 +0000

    Update bar_chart.py

--- Adnan_code/bar_chart.py
+++ Adnan_code/bar_chart.py
@@ -1,21 +1,19 @@
-#dont copy this code
 import dash
-from dash import html
-from dash import dcc
+import dash_html_components as html
+import dash_core_components as dcc
 import pandas as pd
 
-# Read the excel file
-df = pd.read_excel("data_set_prepared.xlsx")
+# Read the second sheet of the excel file
+df = pd.read_excel("data_set_prepared.xlsx", sheet_name=1)
 
 app = dash.Dash()
 
 app.layout = html.Div([
     html.H1("Dash App"),
     dcc.Graph(id='bar-chart',
-              figure={'data': [{'x': df.column_name, 'y': df.value, 'type': 'bar'}],
+              figure={'data': [{'x': df['B'], 'y': df['C'], 'type': 'bar'}],
                       'layout': {'title': 'Bar Chart from Excel Data'}})
 ])
 
 if __name__ == "__main__":
     app.run_server()
-
