231da7aec58eba3f760085657dfbbb1663c669ac
Adnan Kachwala <115113652+naan73@users.noreply.github.com>
2023-02-03 18:08:21 +0000

    Update bar_chart_2.py

--- Adnan_code/bar_chart_2.py
+++ Adnan_code/bar_chart_2.py
@@ -2,25 +2,25 @@ import dash
 from dash import html
 from dash import dcc
 import pandas as pd
+import plotly.express as px
 
 # Read the second sheet of the excel file
 df = pd.read_excel("data_set_prepared.xlsx", sheet_name=1)
 
-# Convert the month column to datetime and set it as the index
-df['Month'] = pd.to_datetime(df['Month'])
-df.set_index('Month', inplace=True)
+# Convert the 'Month' column to a datetime type
+df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m-%D %H-%M-%S')
 
-# Group the data by month and get the mean for each month
-df_grouped = df.groupby(pd.Grouper(freq='M')).mean()
+# Group the data by month and calculate the average of the 3rd and 4th columns
+df_grouped = df.groupby(pd.Grouper(key='Month', freq='M'))[df.columns[1]].mean()
 
+# Create the bar chart
+fig = px.bar(x=df_grouped.index, y=df_grouped)
+
+# Create the Dash app
 app = dash.Dash()
-
 app.layout = html.Div([
-    html.H1("Dash App"),
-    dcc.Graph(id='bar-chart',
-              figure={'data': [{'x': df_grouped.index, 'y': df_grouped['Number of Bicycle Hires.1'], 'type': 'bar'}],
-                      'layout': {'title': 'Monthly Average Usage'}})
+    dcc.Graph(figure=fig)
 ])
 
-if __name__ == "__main__":
-    app.run_server()
+if __name__ == '__main__':
+    app.run_server(debug=True)
