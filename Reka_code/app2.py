7054dfabc1164330207bf0954da7bfc5bd691a1b
reka2002 <88077512+reka2002@users.noreply.github.com>
2023-02-03 18:04:53 +0000

    Update app2.py

--- Reka_code/app2.py
+++ Reka_code/app2.py
@@ -53,7 +53,7 @@ sidebar = html.Div(
         dbc.Nav(
             [
                 dbc.NavLink("Home", href="/", active="exact"),
-                dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                dbc.NavLink("Chart 1", href="/page-1", active="exact"),
                 dbc.NavLink("Page 2", href="/page-2", active="exact"),
             ],
             vertical=True,
@@ -75,10 +75,10 @@ def render_page_content(pathname):
         return html.P("This is the content of the home page!")
     elif pathname == "/page-1":
         return html.Div(
-        [
-            html.H1("404: Not found", className="text-danger"),
+        [ 
             html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
+            html.P(f"Bar chart"),
+            html.Hr(),
             dcc.Graph(id='bar-chart',
               figure={'data': [{'x': df.iloc[:,1], 'y': df.iloc[:,2], 'type': 'bar'}],
                       'layout': {'title': 'Montly Cycle Usage Bar Chart from Excel Data'}})
