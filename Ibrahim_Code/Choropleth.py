f208aae0ed2f39d0c761841d42c07074cf8542bc
IADesai <ibrahim.desai02@gmail.com>
2023-02-16 18:53:36 +0000

    Alhamdulillah

--- Ibrahim_Code/Choropleth.py
+++ Ibrahim_Code/Choropleth.py
@@ -7,18 +7,21 @@ import plotly.express as px
 df = pd.read_excel("data_set_prepared.xlsx", sheet_name=0)
 
 # Group the data by month and average the usage
-grouped = df.groupby('Borough').mean('Total PM 2.5')
+grouped = df.groupby('Borough').sum('Total PM 2.5')
 
 # Load GeoJSON file
 with open('london_boroughs.json') as f:
     geo = json.load(f)
     
+# Create a DataFrame with the borough names and usage values
+data = pd.DataFrame({'locations': grouped.index, 'Total PM 2.5': grouped.iloc[:, 1]})
+
 # Create map figure
-fig = px.choropleth_mapbox(grouped, geojson=geo, color=grouped.iloc[:, 1],
+fig = px.choropleth_mapbox(data, geojson=geo, color='Total PM 2.5',
                            color_continuous_scale='Viridis',
                            mapbox_style='carto-positron',
-                           featureidkey='properties.name', 
-                           locations=grouped.index,
+                           featureidkey='properties.name',
+                           locations='locations',
                            center={"lat": 51.5074, "lon": -0.1278},
                            zoom=10)
 
