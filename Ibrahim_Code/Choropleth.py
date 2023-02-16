9252ded902f10867bd535423ae192ac433fa7de9
IADesai <ibrahim.desai02@gmail.com>
2023-02-16 18:07:58 +0000

    Alhamdulillah

--- Ibrahim_Code/Choropleth.py
+++ Ibrahim_Code/Choropleth.py
@@ -1,30 +1,34 @@
 import dash
 import json
-from dash import html
-from dash import dcc
 import pandas as pd
 import plotly.express as px
 
 # Load data
 df = pd.read_excel("data_set_prepared.xlsx", sheet_name=0)
 
-# Load GeoJSON file
-with open('london_boroughs_proper.geojson') as f:
-    geo = json.load(f)
+# Group the data by month and average the usage
+grouped = df.groupby('Borough').mean('Total PM 2.5')
 
-fig = px.choropleth_mapbox(df, geojson=geo, color='Total PM 2.5',
+# Load GeoJSON file
+with open('london_boroughs.json') as f:
+    geo = json.load(f)
+    
+# Create map figure
+fig = px.choropleth_mapbox(grouped, geojson=geo, color=grouped.iloc[:, 1],
                            color_continuous_scale='Viridis',
                            mapbox_style='carto-positron',
-                           featureidkey='Borough',
-                           locations='Borough',
+                           featureidkey='properties.name', 
+                           locations=grouped.index,
                            center={"lat": 51.5074, "lon": -0.1278},
                            zoom=10)
-# Create Dash app
+
+# Create Dash app layout
 app = dash.Dash()
-app.layout = html.Div(children=[
-    html.H1(children='London Boroughs Choropleth Map'),
-    dcc.Graph(id='london-map', figure=fig)
+app.layout = dash.html.Div([
+    dash.html.H1('London Boroughs Choropleth Map'),
+    dash.dcc.Graph(id='london-map', figure=fig)
 ])
 
+# Run the app
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server()
