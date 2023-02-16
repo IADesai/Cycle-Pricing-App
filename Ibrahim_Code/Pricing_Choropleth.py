5fd5db07662d52c53173e4d6b11475bee5625a8c
IADesai <ibrahim.desai02@gmail.com>
2023-02-16 22:53:27 +0000

    Alhamdulillahirabilalameen

--- Ibrahim_Code/Pricing_Choropleth.py
+++ Ibrahim_Code/Pricing_Choropleth.py
@@ -12,12 +12,24 @@ grouped = df.groupby('Borough').sum('Total PM 2.5')
 # Load GeoJSON file
 with open('london_boroughs.json') as f:
     geo = json.load(f)
-    
+
+print (grouped.iloc[1, 3])
+
+for i in range(0,33):
+    if  grouped.iloc[i, 3]> 0 and grouped.iloc[i, 3] < 25:
+        grouped.iloc[i, 3]=1.65
+    elif grouped.iloc[i, 3] > 25 and grouped.iloc[i, 3] < 50:
+        grouped.iloc[i, 3]=1.45
+    elif grouped.iloc[i, 3] > 50 and grouped.iloc[i, 3] < 75:
+        grouped.iloc[i, 3]=1.25
+    elif grouped.iloc[i, 3] > 75:
+        grouped.iloc[i, 3]=1.00
+
 # Create a DataFrame with the borough names and usage values
-data = pd.DataFrame({'Borough': grouped.index, 'Total PM 2.5': grouped.iloc[:, 3]})
+data = pd.DataFrame({'Borough': grouped.index, 'Cycle Hire Price per Half Hour in Pounds': grouped.iloc[:, 3]})
 
 # Create map figure
-fig = px.choropleth_mapbox(data, geojson=geo, color='Total PM 2.5',
+fig = px.choropleth_mapbox(data, geojson=geo, color='Cycle Hire Price per Half Hour in Pounds',
                            color_continuous_scale='RdYlGn_r',
                            opacity=0.8,
                            mapbox_style='carto-positron',
--- app.py
+++ app.py
@@ -32,6 +32,41 @@ import plotly.graph_objects as go
 df = pd.read_excel("data_set_prepared.xlsx", sheet_name=1)
 dp = pd.read_excel("data_set_prepared.xlsx", sheet_name=0)
 
+def create_pricing_choropleth_map():
+    # Load data
+    df = pd.read_excel("data_set_prepared.xlsx", sheet_name=0)
+
+    # Group the data by month and average the usage
+    grouped = df.groupby('Borough').sum('Total PM 2.5')
+
+    # Load GeoJSON file
+    with open('london_boroughs.json') as f:
+        geo = json.load(f)
+
+    for i in range(0,33):
+        if  grouped.iloc[i, 3]> 0 and grouped.iloc[i, 3] < 25:
+            grouped.iloc[i, 3]=1.65
+        elif grouped.iloc[i, 3] > 25 and grouped.iloc[i, 3] < 50:
+            grouped.iloc[i, 3]=1.45
+        elif grouped.iloc[i, 3] > 50 and grouped.iloc[i, 3] < 75:
+            grouped.iloc[i, 3]=1.25
+        elif grouped.iloc[i, 3] > 75:
+            grouped.iloc[i, 3]=1.00
+
+    # Create a DataFrame with the borough names and usage values
+    data = pd.DataFrame({'Borough': grouped.index, 'Cycle Hire Price per Half Hour in Pounds': grouped.iloc[:, 3]})
+
+    # Create map figure
+    figprice = px.choropleth_mapbox(data, geojson=geo, color='Cycle Hire Price per Half Hour in Pounds',
+                            color_continuous_scale='RdYlGn_r',
+                            opacity=0.8,
+                            mapbox_style='carto-positron',
+                            featureidkey='properties.name',
+                            locations='Borough',
+                            center={"lat": 51.4933, "lon": -0.1},
+                            zoom=9.2)
+    return figprice
+
 # Function for the daily charts
 def create_daily_chart(day_selected):
     # Read the sheet from the excel file
@@ -183,6 +218,17 @@ SIDEBAR_STYLE = {
     "background-color": "#f8f9fa",
 }
 
+home_content = html.Div(
+        [
+            dash.html.H1('TFL Cycle Hire Pricing'),
+            html.P("The Coding Cyclists have tackled TFL's cycle hire pricing, masterminding an algorithm to adjust the price of the cycle hire dependent on hourly and monthly cycle hire data, alongside PM 2.5 pollution levels across the boroughs of London. The aim was to create a price map that increases TFL revenue by promoting cycle hire and taking advantage of rush hour prices, as well as, promoting cycle hire in highly polluted boroughs with hopes to reduce pollution across greater London."),
+            html.Hr(),
+            html.P(f'Choropleth Map Showing Pricing Data for Each Borough of London'),
+            html.Hr(),
+            dcc.Graph(id='london-map', figure=create_pricing_choropleth_map(), style={'width': '1600px', 'height': '700px'})
+        ],
+)
+
 tab1_content = html.Div(style={'display': 'flex'}, children=[
     html.Div(style={'flex': 1}, children=[
         dcc.Graph(id='daily-usage-graph', figure=create_daily_chart(day_selected=0))
@@ -313,7 +359,7 @@ app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
     if pathname == "/":
-        return html.P("This is the content of the home page!")
+        return home_content
     elif pathname == "/page-1":
         return dbc.Tabs(
             [
