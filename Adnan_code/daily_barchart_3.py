d34588278e7fda1a0c46955e3e494db3c566e8b0
Adnan Kachwala <115113652+naan73@users.noreply.github.com>
2023-02-18 23:21:09 +0000

    Update daily_barchart_3.py

--- Adnan_code/daily_barchart_3.py
+++ Adnan_code/daily_barchart_3.py
@@ -56,7 +56,9 @@
 
 import dash
 from dash import html
+import dash_bootstrap_components as dbc
 from dash import dcc
+from dash import Input, Output, dcc
 import pandas as pd
 import plotly.express as px
 
@@ -71,8 +73,8 @@ def create_daily_chart(day_selected):
     df['hour'] = df['TimeString'].dt.hour
 
     # Group the data by hour and sum the usage
-    grouped_sum = df.groupby('hour').sum()
-    grouped_mean = df.groupby('hour').mean()
+    grouped_sum = df.groupby('hour').sum(numeric_only=True)
+    grouped_mean = df.groupby('hour').mean(numeric_only=True)
     grouped = grouped_sum/grouped_mean
 
     # Create the bar chart
@@ -96,7 +98,40 @@ sheet_names = sorted(sheet_names, key=lambda x: pd.to_datetime(x, format="%A, %b
 # Add the '.xlsx' backl into the file
 sheet_names = [name + '.xlsx' for name in sheet_names]
 
-print(sheet_names)
+# Function for the stats panel
+def create_daily_stats(day_selected):
+    df = pd.read_excel("data_set_prepared.xlsx", sheet_name=sheet_names[day_selected])
+    # Convert the 'TimeString' column to a datetime type
+    df['TimeString'] = pd.to_datetime(df['TimeString'], format='%H:%M:%S:%f')
+    
+    # calculate the average usage per hour
+    df['hour'] = df['TimeString'].dt.hour
+    grouped_mean = df.groupby('hour').mean(numeric_only=True)
+    avg_usage_per_hour = round(grouped_mean.iloc[:, 0].mean(), 2)
+
+    # calculate the total usage for the day
+    total_usage = round(df.iloc[:, 1].sum(), 2)
+
+    # create the stats panel
+    stats_panel = dbc.Card(
+        [
+            dbc.CardHeader("Daily Usage Stats"),
+            dbc.CardBody(
+                [
+                    html.P(f"Average Usage per Hour: {avg_usage_per_hour}"),
+                    html.P(f"Total Usage: {total_usage}"),
+                ]
+            ),
+        ],
+        color="light",
+        inverse=True,
+    )
+
+    return stats_panel
+
+
+
+
 # Create the Dash app
 app = dash.Dash()
 app.layout = html.Div(style={'display': 'flex'}, children=[
@@ -104,7 +139,10 @@ app.layout = html.Div(style={'display': 'flex'}, children=[
         dcc.Graph(id='daily-usage-graph', figure=create_daily_chart(day_selected=0))
     ]),
     html.Div(style={'flex': 0.5, 'padding': 20}, children=[
-        dcc.Dropdown(id='day-dropdown', options=[{'label': sheet_name, 'value': i} for i, sheet_name in enumerate(sheet_names)], value=0)
+        dcc.Dropdown(id='day-dropdown', options=[{'label': sheet_name, 'value': i} for i, sheet_name in enumerate(sheet_names)], value=0),
+    
+    html.Br(),
+    html.Div(id="stats-card"),
     ])
 ])
 
@@ -114,6 +152,13 @@ app.layout = html.Div(style={'display': 'flex'}, children=[
 )
 def update_graph(day_selected):
     return create_daily_chart(day_selected)
+
+@app.callback(
+    Output("stats-card", "children"),
+    Input("day-select", "value"),
+)
+def update_daily_stats(day_selected):
+    return create_daily_stats(day_selected=day_selected)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
