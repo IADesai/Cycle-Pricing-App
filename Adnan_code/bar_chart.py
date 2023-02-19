3ac480cff9308548bf3c81cb4732da64356e3363
Adnan Kachwala <115113652+naan73@users.noreply.github.com>
2023-02-19 01:31:54 +0000

    Update bar_chart.py

--- Adnan_code/bar_chart.py
+++ Adnan_code/bar_chart.py
@@ -2,31 +2,22 @@ import dash
 from dash import html
 from dash import dcc
 import pandas as pd
-import plotly.express as px
 
 # Read the second sheet of the excel file
 df = pd.read_excel("data_set_prepared.xlsx", sheet_name=1)
+#look for read csv parse_dates
 
-# Convert the 'Month' column to a datetime type
-df['Month'] = pd.to_datetime(df['Month'], format='%Y-%m-%d %H:%M:%S')
-
-# Extract the month from the datetime object
-df['month'] = df['Month'].dt.month
-
-# Group the data by month and average the usage
-grouped = df.groupby('month').mean()
-
-# Create the bar chart
-fig = px.bar(x=grouped.index, y=grouped.iloc[:, 1])
-
-# Create the Dash app
 app = dash.Dash()
+
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    html.H1("Dash App"),
+    dcc.Graph(id='bar-chart',
+              figure={'data': [{'x': df.iloc[:,1], 'y': df.iloc[:,2], 'type': 'bar'}],
+                      'layout': {'title': 'Montly Cycle Usage Bar Chart from Excel Data'}})
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+if __name__ == "__main__":
+    app.run_server()
 
 
 
