b177f682d4b46876ae2048ed039d1e23e8a3fa26
reka2002 <88077512+reka2002@users.noreply.github.com>
2023-02-19 12:50:04 +0000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,23 +10,38 @@ def test_cycle001_h1_text_equals(dash_duo):
     WHEN the home page is available
     THEN the H1 heading element should include the text 'TFL Cycle Hire Pricing' (not case sensitive)
     """
-    app = import_app(app_file="app")
+    app = import_app(app_file="app_for_testing")
     dash_duo.start_server(app)
     dash_duo.wait_for_element("h1", timeout=4)
     h1_text = dash_duo.find_element("h1").text
     assert h1_text.casefold() == "TFL Cycle Hire Pricing".casefold()
     
-    
+
 def test_cycle002_datedropdowncontainsjul1(dash_duo):
     """
     GIVEN the Dash app is running
-    WHEN the Daily data page has loaded
-    THEN 'Sunday, Jul 1 2018.xlsx' should appear in the area dropdown
+    WHEN the Choropleth Map Showing Pricing Data for Each Borough of London has loaded
+    THEN 'January' should appear in the area dropdown created for month
     """
-    app = import_app(app_file="app")
+    app = import_app(app_file="app_for_testing")
     dash_duo.start_server(app)
-    dash_duo.wait_for_element("#day-dropdown", timeout=14)
-    dash_duo.driver.implicitly_wait(15)
+    dash_duo.wait_for_element("#month-dropdown", timeout=4)
+    dash_duo.driver.implicitly_wait(5)
     assert (
-        0 in dash_duo.find_element("#day-dropdown")
-    ), "0 should appear in the area dropdown"
+        "January" in dash_duo.find_element("#month-dropdown").text
+    ), "'January' should appear in the month dropdown"
+
+    
+# def test_cycle002_datedropdowncontainsjul1(dash_duo):
+#     """
+#     GIVEN the Dash app is running
+#     WHEN the Daily data page has loaded
+#     THEN 'Sunday, Jul 1 2018.xlsx' should appear in the area dropdown
+#     """
+#     app = import_app(app_file="app")
+#     dash_duo.start_server(app)
+#     dash_duo.wait_for_element("#day-dropdown", timeout=14)
+#     dash_duo.driver.implicitly_wait(15)
+#     assert (
+#         0 in dash_duo.find_element("#day-dropdown")
+#     ), "0 should appear in the area dropdown"
