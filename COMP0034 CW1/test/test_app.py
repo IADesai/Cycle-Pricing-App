690e76095754d8224a5c562100d7921b19304606
reka2002 <88077512+reka2002@users.noreply.github.com>
2023-02-21 17:53:00 +0000

    Update test_app.py

--- COMP0034 CW1/test/test_app.py
+++ COMP0034 CW1/test/test_app.py
@@ -15,20 +15,20 @@ from selenium.webdriver.common.keys import Keys
 #     h1_text = dash_duo.find_element("h1").text
 #     assert h1_text.casefold() == "The Coding Cyclists".casefold()
 
+# def test_cycle001_h1_text_equals(dash_duo, app):
+#     """
+#     GIVEN the app is running
+#     WHEN the home page is available
+#     THEN the H1 heading element should include the text 'The Coding Cyclists' (not case sensitive)
+#     """
+#     #app = import_app(app_file="app_for_testing")
+#     #dash_duo.start_server(app)
+#     dash_duo.wait_for_element("H2", timeout=4)
+#     h1_text = dash_duo.find_element("H2").text
+#     assert h1_text.casefold() == "Month of Year:".casefold()
+
+
 def test_cycle001_h1_text_equals(dash_duo, app):
-    """
-    GIVEN the app is running
-    WHEN the home page is available
-    THEN the H1 heading element should include the text 'The Coding Cyclists' (not case sensitive)
-    """
-    #app = import_app(app_file="app_for_testing")
-    #dash_duo.start_server(app)
-    dash_duo.wait_for_element("H2", timeout=4)
-    h1_text = dash_duo.find_element("H2").text
-    assert h1_text.casefold() == "Month of Year:".casefold()
-
-
-def test_cycle002_h2_text_equals(dash_duo, app):
     """
     GIVEN the app is running
     WHEN the home page is available
@@ -36,12 +36,12 @@ def test_cycle002_h2_text_equals(dash_duo, app):
     """
     #app = import_app(app_file="app_for_testing")
     #dash_duo.start_server(app)
-    dash_duo.wait_for_element("h2", timeout=4)
-    h1_text = dash_duo.find_element("h2").text
+    dash_duo.wait_for_element("h1", timeout=4)
+    h1_text = dash_duo.find_element("h1").text
     assert h1_text.casefold() == "TFL Cycle Hire Pricing".casefold()
     
 
-def test_cycle003_monthdropdowncontainsjanuary(dash_duo, app):
+def test_cycle002_monthdropdowncontainsjanuary(dash_duo, app):
     """
     GIVEN the Dash app is running
     WHEN the Choropleth Map Showing Pricing Data for Each Borough of London has loaded
@@ -93,13 +93,11 @@ def test_cycle003_monthdropdowncontainsjanuary(dash_duo, app):
 #         "February" in dash_duo.find_element("#month-dropdown").text
 #     ), "'February' should appear in the month dropdown"
 
-def test_cycle004_monthdropdownchangesdropdown(dash_duo, app):
+def test_cycle003_monthdropdownchangesdropdown(dash_duo, app):
     """
     GIVEN the recycle Dash app is running
     WHEN the area dropdown is changed to Hackney
     THEN the card title for the stats panel is also changed to Hackney.
-
-    Note: using select_dcc_dropdown(elem_or_selector, value=None, index=None) didn't implement the selected value
     """
     #app = import_app(app_file="app_for_testing")
     #dash_duo.start_server(app)
@@ -112,7 +110,7 @@ def test_cycle004_monthdropdownchangesdropdown(dash_duo, app):
         "February" in dash_duo.find_element("#month-dropdown").text
     ), "'February' should appear in the month dropdown"
 
-def test_cycle005_hourdropdowncontains00_06(dash_duo, app):
+def test_cycle004_hourdropdowncontains00_06(dash_duo, app):
     """
     GIVEN the Dash app is running
     WHEN the Choropleth Map Showing Pricing Data for Each Borough of London has loaded
@@ -126,13 +124,11 @@ def test_cycle005_hourdropdowncontains00_06(dash_duo, app):
         "00:00-06:00" in dash_duo.find_element("#hour-dropdown").text
     ), "'00:00-06:00' should appear in the hour dropdown"
 
-def test_cycle006_hourdropdownchangesdropdown(dash_duo, app):
+def test_cycle005_hourdropdownchangesdropdown(dash_duo, app):
     """
     GIVEN the recycle Dash app is running
     WHEN the area dropdown is changed to Hackney
     THEN the card title for the stats panel is also changed to Hackney.
-
-    Note: using select_dcc_dropdown(elem_or_selector, value=None, index=None) didn't implement the selected value
     """
     #app = import_app(app_file="app_for_testing")
     #dash_duo.start_server(app)
@@ -145,7 +141,7 @@ def test_cycle006_hourdropdownchangesdropdown(dash_duo, app):
         "06:00-09:00" in dash_duo.find_element("#hour-dropdown").text
     ), "'06:00-09:00' should appear in the hour dropdown"
 
-def test_cycle007_daydropdowncontainsjul1(dash_duo, app):
+def test_cycle006_daydropdowncontainsjul1(dash_duo, app):
     """
     GIVEN the Dash app is running
     WHEN the Daily data page has loaded
@@ -160,13 +156,11 @@ def test_cycle007_daydropdowncontainsjul1(dash_duo, app):
     ), "'Sunday, Jul 1 2018.xlsx' should appear in the daydropdown"
 
 
-def test_cycle008_daydropdownchangesdropdown(dash_duo, app):
+def test_cycle007_daydropdownchangesdropdown(dash_duo, app):
     """
     GIVEN the recycle Dash app is running
     WHEN the area dropdown is changed to Hackney
     THEN the card title for the stats panel is also changed to Hackney.
-
-    Note: using select_dcc_dropdown(elem_or_selector, value=None, index=None) didn't implement the selected value
     """
     #app = import_app(app_file="app_for_testing")
     #dash_duo.start_server(app)
@@ -178,3 +172,21 @@ def test_cycle008_daydropdownchangesdropdown(dash_duo, app):
     assert (
         "Monday, Jul 2 2018.xlsx" in dash_duo.find_element("#day-dropdown").text
     ), "'Monday, Jul 2 2018.xlsx' should appear in the hour dropdown"
+
+
+def test_cycle007_daydropdownchangesdropdown(dash_duo, app):
+    """
+    GIVEN the recycle Dash app is running
+    WHEN the area dropdown is changed to Hackney
+    THEN the card title for the stats panel is also changed to Hackney.
+    """
+    #app = import_app(app_file="app_for_testing")
+    #dash_duo.start_server(app)
+    dash_duo.wait_for_element("#day-dropdown input", timeout=4)
+    select_input = dash_duo.find_element("#day-dropdown input")
+    updated_input = dash_duo.find_element("#day-dropdown input")[1]
+    updated_input.click()
+    dash_duo.driver.implicitly_wait(5)
+    legend = dash_duo.find_element("#daily-usage-graph .legendtext")
+    updated_legend = legend[1].text
+    assert updated_legend == "%Change (New)"
