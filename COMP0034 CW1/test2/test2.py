3028b5db409bb8653a399ac5c81ce7d026587c5a
Adnan Kachwala <115113652+naan73@users.noreply.github.com>
2023-02-21 23:57:35 +0000

    Update test2.py

--- COMP0034 CW1/test2/test2.py
+++ COMP0034 CW1/test2/test2.py
@@ -39,7 +39,6 @@ def test_cycle001_h2_text_equals(dash_duo, app2):
     assert h2_text.casefold() == "Choropleth Map Showing Pricing Data for Each Borough of London".casefold()
 
 
-<<<<<<< HEAD
 def test_cycle002_h3_text_equals(dash_duo, app):
     """
     GIVEN the app is running
@@ -49,15 +48,4 @@ def test_cycle002_h3_text_equals(dash_duo, app):
     dash_duo.wait_for_element("h3", timeout=4)
     h3_text = dash_duo.find_element("h3").text
     assert h3_text.casefold() == "Month of Year:".casefold()
-=======
-# def test_cycle002_h2_text_equals_2(dash_duo, app2):
-#     """
-#     GIVEN the app is running
-#     WHEN the home page is available
-#     THEN the H2 heading element should include the text 'TFL Cycle Hire Pricing' (not case sensitive)
-#     """
-#     dash_duo.wait_for_element("h2", timeout=4)
-#     h2_text_2 = dash_duo.find_element("h2").text
-#     assert h2_text_2.casefold() == "Month of Year:".casefold()
->>>>>>> 58c9e3add01269dcdaea52b5bb920c442ba6ba7f
     
