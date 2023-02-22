6ba61d997b518e52ac0555a37c8ba925fa32c0fb
reka2002 <88077512+reka2002@users.noreply.github.com>
2023-02-22 01:13:37 +0000

    Update test_app.py

--- COMP0034 CW1/test/test_app.py
+++ COMP0034 CW1/test/test_app.py
@@ -2,6 +2,176 @@ from dash.testing.application_runners import import_app
 from selenium.webdriver.common.keys import Keys
 
 # py -m pytest -v
+def test_cycle001_h1_text_equals(dash_duo, app):
+    """
+    GIVEN the app is running
+    WHEN the home page is available
+    THEN the H2 heading element should include the text 'TFL Cycle Hire Pricing' (not case sensitive)
+    """
+    dash_duo.wait_for_element("h1", timeout=4)
+    h1_text = dash_duo.find_element("h1").text
+    assert h1_text.casefold() == "TFL Cycle Hire Pricing".casefold()
+
+def test_cycle002_id_d2_text_equals(dash_duo, app):
+    """
+    GIVEN the app is running
+    WHEN the home page is available
+    THEN the H2 heading element should include the text 'TFL Cycle Hire Pricing' (not case sensitive)
+    """
+    dash_duo.wait_for_element("#id-d2", timeout=4)
+    id_d2_text = dash_duo.find_element("#id-d2").text
+    assert id_d2_text.casefold() == "Month of Year:".casefold()
+
+def test_cycle003_id_title1_text_equals(dash_duo, app):
+    """
+    GIVEN the app is running
+    WHEN the home page is available
+    THEN the element with id = '#id-title1'should include the text 'Daily Data' (not case sensitive)
+    """
+    dash_duo.wait_for_element("#id-title1", timeout=4)
+    id_title_1_text = dash_duo.find_element("#id-title1").text
+    assert id_title_1_text.casefold() == "Daily Data".casefold()
+
+def test_cycle004_id_title2_text_equals(dash_duo, app):
+    """
+    GIVEN the app is running
+    WHEN the home page is available
+    THEN the element with id = '#id-title2'should include the text 'Monthly Data' (not case sensitive)
+    """
+    dash_duo.wait_for_element("#id-title2", timeout=4)
+    id_title_2_text = dash_duo.find_element("#id-title2").text
+    assert id_title_2_text.casefold() == "Monthly Data".casefold()
+
+def test_cycle005_id_title3_text_equals(dash_duo, app):
+    """
+    GIVEN the app is running
+    WHEN the home page is available
+    THEN the element with id = '#id-title3'should include the text 'Pollution Data' (not case sensitive)
+    """
+    dash_duo.wait_for_element("#id-title3", timeout=4)
+    id_title_3_text = dash_duo.find_element("#id-title3").text
+    assert id_title_3_text.casefold() == "Pollution Data".casefold()
+
+
+def test_cycle006_monthdropdowncontainsjanuary(dash_duo, app):
+    """
+    GIVEN the Dash app is running
+    WHEN the Choropleth Map Showing Pricing Data for Each Borough of London has loaded
+    THEN 'January' should appear in the area dropdown created for months
+    """
+    dash_duo.wait_for_element("#month-dropdown", timeout=4)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "January" in dash_duo.find_element("#month-dropdown").text
+    ), "'January' should appear in the month dropdown"
+
+def test_cycle007_monthdropdownchangesdropdown(dash_duo):
+    """
+    GIVEN the recycle Dash app is running
+    WHEN the area dropdown is changed to Hackney
+    THEN the card title for the stats panel is also changed to Hackney.
+
+    Note: using select_dcc_dropdown(elem_or_selector, value=None, index=None) didn't implement the selected value
+    """
+    app = import_app(app_file="app_for_testing")
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#month-select", timeout=4)
+    select_input = dash_duo.find_element("#month-select input")
+    select_input.send_keys("February")
+    select_input.send_keys(Keys.RETURN)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "February" in dash_duo.find_element("#month-select").text
+    ), "'February' should appear in the month dropdown"
+
+def test_cycle008_monthdropdownchangesdropdown(dash_duo):
+    """
+    GIVEN the recycle Dash app is running
+    WHEN the area dropdown is changed to Hackney
+    THEN the card title for the stats panel is also changed to Hackney.
+
+    Note: using select_dcc_dropdown(elem_or_selector, value=None, index=None) didn't implement the selected value
+    """
+    app = import_app(app_file="app_for_testing")
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#month-dropdown", timeout=4)
+    select_input = dash_duo.find_element("#month-dropdown")
+    select_input.send_keys("February")
+    select_input.send_keys(Keys.RETURN)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "February" in dash_duo.find_element("#month-dropdown").text
+    ), "'February' should appear in the month dropdown"
+
+def test_cycle009_monthdropdownchangesdropdown(dash_duo, app):
+    """
+    GIVEN the recycle Dash app is running
+    WHEN the area dropdown is changed to Hackney
+    THEN the card title for the stats panel is also changed to Hackney.
+    """
+    dash_duo.wait_for_element("#month-dropdown", timeout=4)
+    select_input = dash_duo.find_element("#month-dropdown input")
+    select_input.send_keys("February")
+    select_input.send_keys(Keys.RETURN)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "February" in dash_duo.find_element("#month-dropdown").text
+    ), "'February' should appear in the month dropdown"
+
+def test_cycle010_hourdropdowncontains00_06(dash_duo, app):
+    """
+    GIVEN the Dash app is running
+    WHEN the Choropleth Map Showing Pricing Data for Each Borough of London has loaded
+    THEN 'January' should appear in the area dropdown created for months
+    """
+    dash_duo.wait_for_element("#hour-dropdown", timeout=4)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "00:00-06:00" in dash_duo.find_element("#hour-dropdown").text
+    ), "'00:00-06:00' should appear in the hour dropdown"
+
+def test_cycle011_hourdropdownchangesdropdown(dash_duo, app):
+    """
+    GIVEN the recycle Dash app is running
+    WHEN the area dropdown is changed to Hackney
+    THEN the card title for the stats panel is also changed to Hackney.
+    """
+    dash_duo.wait_for_element("#hour-dropdown", timeout=4)
+    select_input = dash_duo.find_element("#hour-dropdown input")
+    select_input.send_keys("06:00-09:00")
+    select_input.send_keys(Keys.RETURN)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "06:00-09:00" in dash_duo.find_element("#hour-dropdown").text
+    ), "'06:00-09:00' should appear in the hour dropdown"
+
+def test_cycle012_daydropdowncontainsjul1(dash_duo, app):
+    """
+    GIVEN the Dash app is running
+    WHEN the Daily data page has loaded
+    THEN 'Sunday, Jul 1 2018.xlsx' should appear in the area dropdown
+    """
+    dash_duo.wait_for_element("#day-dropdown", timeout=4)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "Sunday, Jul 1 2018.xlsx" in dash_duo.find_element("#day-dropdown").text
+    ), "'Sunday, Jul 1 2018.xlsx' should appear in the daydropdown"
+
+
+def test_cycle013_daydropdownchangesdropdown(dash_duo, app):
+    """
+    GIVEN the recycle Dash app is running
+    WHEN the area dropdown is changed to Hackney
+    THEN the card title for the stats panel is also changed to Hackney.
+    """
+    dash_duo.wait_for_element("#day-dropdown", timeout=4)
+    select_input = dash_duo.find_element("#day-dropdown input")
+    select_input.send_keys("Monday, Jul 2 2018.xlsx")
+    select_input.send_keys(Keys.RETURN)
+    dash_duo.driver.implicitly_wait(5)
+    assert (
+        "Monday, Jul 2 2018.xlsx" in dash_duo.find_element("#day-dropdown").text
+    ), "'Monday, Jul 2 2018.xlsx' should appear in the day dropdown"
 
 
 # # # def test_cycle001_h1_text_equals(dash_duo, app):
@@ -29,15 +199,7 @@ from selenium.webdriver.common.keys import Keys
 # # #     assert h1_text.casefold() == "Month of Year:".casefold()
 
 
-# # def test_cycle001_h1_text_equals(dash_duo, app):
-# #     """
-# #     GIVEN the app is running
-# #     WHEN the home page is available
-# #     THEN the H2 heading element should include the text 'TFL Cycle Hire Pricing' (not case sensitive)
-# #     """
-# #     dash_duo.wait_for_element("h1", timeout=4)
-# #     h1_text = dash_duo.find_element("h1").text
-# #     assert h1_text.casefold() == "TFL Cycle Hire Pricing".casefold()
+
     
 
 # # def test_cycle002_monthdropdowncontainsjanuary(dash_duo, app):
@@ -161,35 +323,35 @@ from selenium.webdriver.common.keys import Keys
 # #     ), "'Monday, Jul 2 2018.xlsx' should appear in the day dropdown"
 
 
-# # def test_cycle008_daydropdownchangesdropdown(dash_duo, app):
-# #     """
+# def test_cycle008_daydropdownchangesdropdown(dash_duo, app):
+#     """
 
-# #     """
-# #     dash_duo.wait_for_element("#day-dropdown", timeout=40)
-# #     select_input = dash_duo.find_element("#day-dropdown input")
-# #     updated_input = select_input[1]
+#     """
+#     dash_duo.wait_for_element("#day-dropdown", timeout=40)
+#     select_input = dash_duo.find_element("#day-dropdown input")
+#     updated_input = select_input[1]
 
-# # #    updated_input = dash_duo.find_element("#day-dropdown input")[1]
-# #     updated_input.click()
-# #     dash_duo.driver.implicitly_wait(50)
-# #     title = dash_duo.find_element("#daily-usage-graph.title")
-# #     updated_title = title[1].text
-# #     assert updated_title == "%Change (New)"
+# #    updated_input = dash_duo.find_element("#day-dropdown input")[1]
+#     updated_input.click()
+#     dash_duo.driver.implicitly_wait(50)
+#     title = dash_duo.find_element("#daily-usage-graph.title")
+#     updated_title = title[1].text
+#     assert updated_title == "%Change (New)"
 
 
-# # def test_cycle009_daydropdownchangesdropdown(dash_duo, app):
-# #     """
-# #     GIVEN the recycle Dash app is running
-# #     WHEN the area dropdown is changed to Hackney
-# #     THEN the card title for the stats panel is also changed to Hackney.
-# #     """
-# #     dash_duo.wait_for_element("#day-dropdown", timeout=4)
-# #     select_input = dash_duo.find_element("#day-dropdown input")
-# #     select_input.send_keys("Monday, Jul 2 2018.xlsx")
-# #     select_input.send_keys(Keys.RETURN)
-# #     dash_duo.driver.implicitly_wait(5)
-# #     title = dash_duo.find_element("#daily-usage-graph .titletext")
-# #     assert title.text == "Cycle Usage for Monday, Jul 2 2018.xlsx"
+# def test_cycle009_daydropdownchangesdropdown(dash_duo, app):
+#     """
+#     GIVEN the recycle Dash app is running
+#     WHEN the area dropdown is changed to Hackney
+#     THEN the card title for the stats panel is also changed to Hackney.
+#     """
+#     dash_duo.wait_for_element("#day-dropdown", timeout=4)
+#     select_input = dash_duo.find_element("#day-dropdown input")
+#     select_input.send_keys("Monday, Jul 2 2018.xlsx")
+#     select_input.send_keys(Keys.RETURN)
+#     dash_duo.driver.implicitly_wait(5)
+#     title = dash_duo.find_element("#daily-usage-graph .titletext")
+#     assert title.text == "Cycle Usage for Monday, Jul 2 2018.xlsx"
 
 # def test_day_dropdown_changes_data_displayed(dash_duo, app):
 #     """
@@ -240,53 +402,3 @@ from selenium.webdriver.common.keys import Keys
 #     assert h2_text.casefold() == "Choropleth Map Showing Pricing Data for Each Borough of London".casefold()
 
 
-def test_cycle001_id_d1_text_equals(dash_duo, app):
-    """
-    GIVEN the app is running
-    WHEN the home page is available
-    THEN the H2 heading element should include the text 'TFL Cycle Hire Pricing' (not case sensitive)
-    """
-    dash_duo.wait_for_element("#id-d1", timeout=4)
-    id_d1_text = dash_duo.find_element("#id-d1").text
-    assert id_d1_text.casefold() == "Time of Day (24 Hour Clock):".casefold()
-
-def test_cycle002_id_d2_text_equals(dash_duo, app):
-    """
-    GIVEN the app is running
-    WHEN the home page is available
-    THEN the H2 heading element should include the text 'TFL Cycle Hire Pricing' (not case sensitive)
-    """
-    dash_duo.wait_for_element("#id-d2", timeout=4)
-    id_d2_text = dash_duo.find_element("#id-d2").text
-    assert id_d2_text.casefold() == "Month of Year:".casefold()
-
-
-def test_cycle003_id_title1_text_equals(dash_duo, app):
-    """
-    GIVEN the app is running
-    WHEN the home page is available
-    THEN the element with id = '#id-title1'should include the text 'Daily Data' (not case sensitive)
-    """
-    dash_duo.wait_for_element("#id-title1", timeout=4)
-    id_title_1_text = dash_duo.find_element("#id-title1").text
-    assert id_title_1_text.casefold() == "Daily Data".casefold()
-
-def test_cycle004_id_title2_text_equals(dash_duo, app):
-    """
-    GIVEN the app is running
-    WHEN the home page is available
-    THEN the element with id = '#id-title2'should include the text 'Monthly Data' (not case sensitive)
-    """
-    dash_duo.wait_for_element("#id-title2", timeout=4)
-    id_title_2_text = dash_duo.find_element("#id-title2").text
-    assert id_title_2_text.casefold() == "Monthly Data".casefold()
-
-def test_cycle005_id_title3_text_equals(dash_duo, app):
-    """
-    GIVEN the app is running
-    WHEN the home page is available
-    THEN the element with id = '#id-title3'should include the text 'Pollution Data' (not case sensitive)
-    """
-    dash_duo.wait_for_element("#id-title3", timeout=4)
-    id_title_3_text = dash_duo.find_element("#id-title3").text
-    assert id_title_3_text.casefold() == "Pollution Data".casefold()
